4e53c39b7cde9def85853f42e3449c60cd4dee82
andresarad1 <andresarad@misena.edu.co>
2023-10-03 15:35:56 -0500

    Add CICD settings 2

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "cypress";
 
 module.exports = defineConfig({
-  e2e: {
-    video: false,
-    screenshotOnRunFailure: false,
-    setupNodeEvents(on, config) {},
+  video: false,
+  screenshotOnRunFailure: false,
+  e2e: {    
+    setupNodeEvents(on, config) {
+
+    },
     baseUrl: "https://www.demoblaze.com/index.html",
     defaultCommandTimeout: 20000,
     pageLoadTimeout: 20000,   
    
   },
+ 
 });
